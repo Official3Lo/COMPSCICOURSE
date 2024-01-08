b3ee3ec0dff461469fb41124ebab622402bca1df
Sit Lo <141769635+Official3Lo@users.noreply.github.com>
2024-01-08 11:13:21 -0800

    Simple Output Demonstration
    Created a basic program that displays I am capable of coding basic outputs in C++.

---
+++ Assignment 1-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Program 1: " << endl;
+	cout << "+------------------------+" << endl;
+	cout << "|                        |" << endl;
+	cout << "|      Good Morning!     |" << endl;
+	cout << "|                        |" << endl;
+	cout << "+------------------------+" << endl;
+	cout << "                          " << endl;
+	cout << "\nProgram 2: " << endl;
+	cout << "==========================" << endl;
+	cout << "Textbook:     Cost:       " << endl;
+	cout << "==========================" << endl;
+	cout << "MATH          $52.78      " << endl;
+	cout << "CSCI          $125.56     " << endl;
+	cout << "English       $5.26       " << endl;
+	cout << "Biology       $158.72     " << endl;
+	cout << "==========================" << endl;
+
+	cout << "\n\n\n========================\nSIT© Programming Inc.\n========================" << endl;
+	return 0;
+}
