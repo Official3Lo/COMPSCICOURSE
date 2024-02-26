521832f8ced32bfbe14168c9370e80c62daf3d44
Sit Lo <141769635+Official3Lo@users.noreply.github.com>
2024-02-25 22:30:25 -0800

    SimpleWhileLoopAddition

---
+++ SimpleSumforWhileloop.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<iomanip>
+#include<climits>
+
+using namespace std;
+
+int main()
+{
+
+	cout <<"+-------------------------------+\n"
+		<< "|                               |\n"
+		<< "|      2.6 - Find the Sum       |\n" 
+		<< "|          Program		        |\n"
+		<< "+-------------------------------+\n\n" << endl;
+	
+	int sum, a;
+	sum = 0;
+	int i = 0;
+
+
+	while (i < 3) {
+		cout << "Please enter an integer: " << endl;
+		cin >> a;
+		sum = sum + a;
+		i++;
+	}
+	
+	cout << "sum of the numbers is: " << sum << endl;
+    
+  
+
+
+        cout << "\n========================\nSIT© Programming Inc.\n========================" << endl;
+        return 0;
+    
+}
