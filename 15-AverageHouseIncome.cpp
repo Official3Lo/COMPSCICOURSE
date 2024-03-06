7bf976ae7620037efc6632b6fbaf3eeb7af5ecb8
Sit Lo <141769635+Official3Lo@users.noreply.github.com>
2024-03-06 07:23:59 -0800

    AverHouseIncomeCalculator

---
+++ 15-AverageHouseIncome.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+#include<iomanip>
+#include<climits>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+
+	cout <<"+-------------------------------+\n"
+		<< "|                               |\n"
+		<< "| 2.7- Average Household Income |\n" 
+		<< "|          Program		        |\n"
+		<< "+-------------------------------+\n\n" << endl;
+	
+	//Data variable
+	int x,j;
+	int averageHI = 0;
+	int i = 0;
+
+	//House data option prompt
+	cout << "Enter the number of houses you gathered data from: " << endl;
+	cin >> j;
+
+	//input of data
+	while (i < j) {
+		cout << "Please enter the house income: " << endl;
+		cin >> x;
+		averageHI = averageHI+x;
+		i++;
+	}
+	
+	//final calculation and output of household average income 
+	averageHI = averageHI / j;
+
+	cout << "sum of the numbers is: " << averageHI << endl;
+    
+  
+
+
+        cout << "\n========================\nSIT© Programming Inc.\n========================" << endl;
+        return 0;
+    
+}
